0fb37faa6d791f1c7e1017ecc8bc457cb22e480f
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-12-13 20:02:21 -0800

    2. Programmatically centered the "Swoosh" logo in the View Controller.

--- Swish-Swoosh/ViewController.swift
+++ Swish-Swoosh/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+        
+        backgroundImage.frame = view.frame
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
