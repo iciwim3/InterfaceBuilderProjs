cea7ad439df1613fdc0f3907d47e92b82013d242
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-12-14 19:36:54 -0800

    9. Created Player struct and added to the Model group for better organization of stored properties.
    10. Created a function for selected league on the
    LeagueVC that provides the desired league and
    enables the "Next" button after tapped.
    
    11. Created player variable in SkillVC to pass data
    from LeagueVC and the SkillVC.
    
    12. Implemented the prepareForSegue function
    from the LeagueVC to the SkillVC.

---
+++ Swish-Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swish-Swoosh
+//
+//  Created by Sain-R Edwards Jr. on 12/14/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
